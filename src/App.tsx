6fab87d6a9ce5eda06afc911779d126cd5ed8908
TransposeDev <tofexzyno@gmail.com>
2025-09-16 18:17:52 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
           <Route
             path="*"
             element={
-              <div className="flex items-center justify-center w-full h-full text-4xl sm:text-5xl font-bold">
+              <div className="flex items-center justify-center w-full h-[80vh] text-4xl sm:text-5xl font-bold">
                 Coming Soon!!!
               </div>
             }
