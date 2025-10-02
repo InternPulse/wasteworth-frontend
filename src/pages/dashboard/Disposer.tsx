f293c77d3340c1e86248ba19504e5d2f8f4955e7
TransposeDev <tofexzyno@gmail.com>
2025-10-02 14:46:19 +0100

    Get dashboard data

--- src/pages/dashboard/Disposer.tsx
+++ src/pages/dashboard/Disposer.tsx
@@ -1,23 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import MainCard from "../../components/dashboards/disposer/MainCard";
 import RecentPost from "../../components/dashboards/disposer/RecentPost";
 import Referral from "../../components/dashboards/disposer/Referral";
 import axios from "axios";
-import useStore from "../../../store/store";
 
 export default function Disposer() {
-  const { tokens } = useStore();
-  useEffect(() => {
-    async () => {
-      await axios.get(
+  const [_, setData] = useState({});
+  const fetch = async () => {
+    console.log("fetching...");
+    try {
+      const res = await axios.get(
         "https://wasteworth-backend-django.onrender.com/api/v1/users/user-dashboard/",
         {
           headers: {
-            Authorization: `Bearer ${tokens.access_token}`,
+            Authorization: `Bearer ${
+              JSON.parse(localStorage.getItem("tokens") ?? "").access
+            }`,
           },
         }
       );
-    };
+      setData(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetch();
   }, []);
   return (
     <div className="w-full">
