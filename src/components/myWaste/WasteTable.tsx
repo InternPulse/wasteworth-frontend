56df04c9bb9affd0f63604e6b6595c35c7647bae
TransposeDev <tofexzyno@gmail.com>
2025-10-09 19:48:41 +0100

    Render disposer recent posts

--- src/components/myWaste/WasteTable.tsx
+++ src/components/myWaste/WasteTable.tsx
@@ -1,57 +1,58 @@
 import type { Tdata } from "@/types";
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import { useMemo } from 'react';
+import { useMemo } from "react";
 
 const getStatusClass = (status: string) => {
   switch (status) {
-    case "Completed":
+    case "completed":
       return "text-green-700";
-    case "Accepted":
+    case "accepted":
       return "text-blue-700";
-    case "Pending":
+    case "pending":
       return "text-gray-500";
     default:
       return "text-red-700";
   }
 };
 
- 
-
 const WasteTable = ({ item }: { item: Tdata }) => {
-
   const locationToDisplay = useMemo(() => {
-        const location = item.pickup_location;
-        if (typeof location === 'string') {
-            return location;
-        }
-        if (location && typeof location === 'object' && location.address) {
-            return location.address;
-        }
-        if (location && typeof location === 'object' && location.city) {
-            return location.city;
-        }
-        return 'N/A';
-    }, [item.pickup_location]);
+    const location = item.pickup_location;
+    if (typeof location === "string") {
+      return location;
+    }
+    if (location && typeof location === "object" && location.address) {
+      return location.address;
+    }
+    if (location && typeof location === "object" && location.city) {
+      return location.city;
+    }
+    return "N/A";
+  }, [item.pickup_location]);
 
-  
   return (
     <tr key={item._id} className="text-[14px]">
-      <td className="px-4 border border-gray-200 py-2 hidden lg:table-cell">
-        {item._id}
+      <td className="px-4 border border-gray-200 py-2 hidden lg:table-cell line-clamp-2">
+        {item.id.slice(0, 5)}...
       </td>
-      <td className="px-4 border border-gray-200 py-2 hidden lg:table-cell">
+      <td className="px-4 text-center border border-gray-200 py-2 hidden lg:table-cell">
         {item.quantity}
       </td>
-      <td className="px-4 border border-gray-200 py-2"> {item.created_at}</td>
-      <td className="px-4 border border-gray-200 py-2 hidden md:table-cell">
+      <td className="px-4 border border-gray-200 py-2 text-center">
+        {" "}
+        {item.created_at.slice(0, 10)}
+      </td>
+      <td className="px-4 border border-gray-200 py-2 hidden md:table-cell text-center">
         {locationToDisplay}
       </td>
-      <td className="px-4 border border-gray-200 py-2">
+      <td className="px-4 border border-gray-200 py-2 text-center">
         {item.reward_estimate}
       </td>
-      <td className="px-4 border border-gray-200 py-2 flex items-center justify-between gap-2">
-        <span className={` ${getStatusClass(item.status)}`}>{item.status}</span>
+      <td className="px-4 border h-full border-gray-200 py-2 flex items-center justify-between gap-2">
+        <span className={` ${getStatusClass(item.status)} `}>
+          {item.status}
+        </span>
         <Link to="#">
           <FaArrowRight className="text-gray-400" />
         </Link>
